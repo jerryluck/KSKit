e4799841eb2b156bd5cb3b043935139dad2d51d9
Jerry <gaoguang2008@126.com>
2015-10-13 14:54:08 +0800

    "2.7"

--- pod_update.sh
+++ pod_update.sh
@@ -3,16 +3,16 @@ echo -n "enter the version: "
 read version
 
 echo -e "\033[32mthe version is: "$version"\033[0m"
-echo -e "\033[33mgit add -A && git commit -m '"'$version'"'\033[0m"
+echo -e "\033[32mgit add -A && git commit -m "'"'$version'"'"\033[0m"
 git add -A && git commit -m '"'$version'"'
-echo -e "\033[33mgit tag "'"$version"'"\033[0m"
+echo -e "\033[32mgit tag ""'"$version"'""\033[0m"
 git tag "'"$version"'"
 
 echo -e "\033[33mgit push --tags\033[0m"
 git push --tags
 echo -e "\033[33mgit push origin master\033[0m"
 git push origin master
-echo -e "\033[33mset the new version to "$version"\033[0m"
+echo -e "\033[32mset the new version to "$version"\033[0m"
 set the new version to $version
 echo -e "\033[32mset the new tag to "$version"\033[0m"
 set the new tag to $version
