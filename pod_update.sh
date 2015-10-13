6f03329c4f96d6e2d56b6282e58c0b4ebf913922
Jerry <gaoguang2008@126.com>
2015-10-13 14:20:22 +0800

    "2.4"

--- pod_update.sh
+++ pod_update.sh
@@ -2,7 +2,7 @@
 echo -n "enter the version: "
 read version
 
-echo  "the version is: "$version
+echo -e "\033[33mthe version is: "$version"\033[0m"
 git add -A && git commit -m '"'$version'"'
 git tag '"'$version'"'
 echo -e "\033[33mpush tag to git host...\033[0m"
@@ -13,4 +13,6 @@ set the new version to $version
 set the new tag to $version
 echo  -e "\033[33mpush to pod trunk host...\033[0m"
 pod trunk push KSKit.podspec
+echo -e "\033[33setup pod\033[0m"
+pod setup
 echo -e "\033[33m结束\033[0m"
